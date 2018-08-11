1bd5224f8afb2cb7b62660ad14d24348a6916516
gitstephanie <s.dejong@mail.com>
2018-08-11 10:13:54 +0200

    Hero object

--- BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,12 @@
 // Write your JS here
+var hero = {
+    name : "I",
+    heroic: true,
+    inventory : [],
+    health : 10,
+    weapon : { 
+            type : 'knife',
+            damage: 2,
+    };
+};
+
