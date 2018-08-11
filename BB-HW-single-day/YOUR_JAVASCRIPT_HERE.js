ec180d62ee07ff5daa846156391fdfbb70b0dd93
gitstephanie <s.dejong@mail.com>
2018-08-11 11:49:16 +0200

    Rest function and then some :)

--- BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
@@ -7,6 +7,31 @@ var hero = {
     weapon : { 
             type : 'knife',
             damage: 2,
-    };
+    }
+};
+
+function rest (hero){
+    hero.health = 10;
+    return hero;
+};
+
+function resetHealth(){
+    hero.health = 10;
+};
+
+function pickUpItem(hero, weapon){
+    hero.inventory.push(weapon);
+};
+
+function addDagger(){
+    hero.inventory.push(hero.weapon = {type: dagger, damage: 2})
+};
+
+function equipWeapon(hero){
+    if (hero.inventory === null){
+
+    } else {
+    hero.weapon = hero.inventory[0];
+    }
 };
 
